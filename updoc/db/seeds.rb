f8a0621adf21eb4b211f505ffa9aba3d54aac64b
Farouk Jeffar <fjeffar1@binghamton.edu>
2018-03-26 20:38:25 +0000

    DB update and javascipt

--- updoc/db/seeds.rb
+++ updoc/db/seeds.rb
@@ -2,8 +2,26 @@ hospitals = [{:name=> 'Lourd', :pnumber=> '6072386492', :city=> 'Binghamton', :s
     	  {:name=> 'Vestal Hospital', :pnumber=> '6070006875', :city=> 'Vestal', :street=> 'Park', :hnumber=> 32},
     	  {:name=> 'University Hospital', :pnumber=> '6073216875', :city=> 'Binghamton', :street=> 'Vestal Parkway West', :hnumber=> 54}
   	 ]
+
+pharmacies = [{:name=> 'Ab Pharma', :pnumber=> '12345678', :city=> 'Binghamton', :street=> 'Riverside Drive', :hnumber=> 27},
+    	  {:name=> 'Jeff PHY', :pnumber=> '607345582', :city=> 'Vestal', :street=> 'East drive', :hnumber=> 46},
+    	  {:name=> 'University Pharmacy', :pnumber=> '607125986', :city=> 'Binghamton', :street=> 'Vestal Parkway West', :hnumber=> 56}
+  	 ]
+  	 
+credentials = [{:username=> 'Aditya', :password=> 'Arsenal4'},
+    	  {:username=> 'Jeff', :password=> 'Realmadrid1'},
+    	  {:username=> 'Joe', :password=> 'Mufc19'}
+  	 ]
   	 
 
 hospitals.each do |hospital|
   Hospital.create!(hospital)
+end
+
+pharmacies.each do |pharmacy|
+ Pharmacy.create!(pharmacy)
+end
+
+credentials.each do |credential|
+ Credential.create!(credential)
 end
