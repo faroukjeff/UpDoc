56c8aa93c2cf9ffe7ac7a9bfc70b84a3693b4491
Farouk Jeffar <fjeffar1@binghamton.edu>
2018-03-28 06:42:45 +0000

    Homepage Done

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   resources :contacts,
             controller: "contact_us/contacts",
             only:       [:new, :create]
+
   #get "contact-us" => "contact_us/contacts#new", as: :contact_us
   
   # The priority is based upon order of creation: first created -> highest priority.
