cacfa0d4b7539bacfa11f07e48022fc2a2770c49
joe lyons <jlyons10@binghamton.edu>
2018-04-25 17:50:09 +0000

    Setting up some basic rspec. Needs refinement

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -9,4 +9,15 @@ RSpec.describe SessionsController, type: :controller do
     end
   end
 
+  describe "doctor log in" do
+    it "is unsuccessful" do
+      post :show, {:session => {:username => 'email@bin.com', :password => 'invalid'} }
+      expect(response).to redirect_to('/login')
+    end
+    it "is successful" do
+      post :show, {:session => {:username => 'Aditya@bin.com', :password => 'Arsenal4'} }
+      expect(response).to redirect_to('/doctorprofile')
+    end
+  end
+
 end
