a69b62f5e272d90efa4b45c70ad4f6798f77a3f3
joe lyons <jlyons10@binghamton.edu>
2018-05-07 20:25:07 +0000

    Resolved rspec issue

--- spec/controllers/signup_controller_spec.rb
+++ spec/controllers/signup_controller_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe SignupController, type: :controller do
       expect(response).to redirect_to(signup_signup_path)
     end
     it "receives incomplete fields" do
-      post :add, {:session => {:username => nil, :first_name => "New", :last_name_string => "User", :address => "11 Eleven St", :pnumber => "D12", :Healthcare => "waypoint", :password => "pass", :confirmPassword => "pass2"} }
+      post :add, {:session => {:username => "", :first_name => "New", :last_name_string => "User", :address => "11 Eleven St", :pnumber => "D12", :Healthcare => "waypoint", :password => "pass", :confirmPassword => "pass2"} }
       expect(response).to redirect_to(signup_signup_path)
     end
     it "is successful" do
