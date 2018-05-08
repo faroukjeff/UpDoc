8e95bfa84c089cb1f87f42e62617a8a4ad3ba1e8
joe lyons <jlyons10@binghamton.edu>
2018-05-08 20:18:11 +0000

    Removed .create! calls in rspec

--- spec/controllers/docappt_controller_spec.rb
+++ spec/controllers/docappt_controller_spec.rb
@@ -4,19 +4,16 @@ RSpec.describe DocapptController, type: :controller do
 
   describe "create appointment slot for a doctor" do
     it "is successful" do
-      @d = Credential.create!({:username=> 'Aditya@bin.com', :password=> 'Arsenal4', :usertype=> 'D'})
-      post :add, {:session => {:docid => 'Aditya@bin.com', :slot => '9:00'} }
-      @a = Appointment.new({:pid=>nil, :did=>"Aditya@bin.com", :slot=>"9:00", :av=>"A"})
+      post :add, {:session => {:docid => 'Aditya@bin.com', :slot => '10:00'} }
       
       expect(response).to redirect_to(admin_admin_path)
-      expect(Appointment.all[0].pid).to eq(nil)
-      expect(Appointment.all[0].did).to eq("Aditya@bin.com")
-      expect(Appointment.all[0].slot).to eq("9:00")
-      expect(Appointment.all[0].av).to eq("A")
+      expect(Appointment.all[2].pid).to eq(nil)
+      expect(Appointment.all[2].did).to eq("Aditya@bin.com")
+      expect(Appointment.all[2].slot).to eq("10:00")
+      expect(Appointment.all[2].av).to eq("A")
       expect(flash[:notice]).to have_content("Doctor Added To Appointment Table")
     end
     it "has invalid field" do
-      Credential.create!({:username=> 'Aditya@bin.com', :password=> 'Arsenal4', :usertype=> 'D'})
       post :add, {:session => {:docid => "John@doctors.com", :slot => nil} }  
       expect(response).to redirect_to(admin_admin_path)
       expect(flash[:notice]).to have_content("Doctor Does not exist")
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -15,9 +15,7 @@ RSpec.describe SessionsController, type: :controller do
       expect(response).to redirect_to('/login')
     end
     it "is successful" do
-      @cred = {:username => 'Aditya@bin.com', :password => 'Arsenal4', :usertype => "D"}
-      Credential.create!(@cred)
-      post :show, {:session => @cred }
+      post :show, {:session => {:username => 'Aditya@bin.com', :password => 'Arsenal4', :usertype => "D"} }
       #expect(page.body).to match("Your Profile")
       expect(response).to redirect_to(profile_doctorprofile_path(:params1 => 'Aditya@bin.com'))
     end
@@ -41,11 +39,7 @@ RSpec.describe SessionsController, type: :controller do
       expect(response).to redirect_to('/login')
     end
     it "is successful" do
-      #post :show, {:session => {:username => 'Jeff@bin.com', :password => 'Realmadrid1'} }
-      Profile.create!({:username=> "Jeff@bin.com",:first_name=>"Jeff",:last_name_string=>"Baker",:address=>"198 Main Street, Binghamton NY",:pnumber=>"P0342567",:Healthcare=>"Geo Blue",:password=>"Realmadrid1",:docid=>"Aditya@bin.com"}) 
-      @cred = {:username => 'Jeff@bin.com', :password => 'Realmadrid1'}
-      Credential.create!(@cred)
-      post :show, {:session => @cred }
+      post :show, {:session => {:username => 'Jeff@bin.com', :password => 'Realmadrid1'} }
       expect(response).to have_http_status(:success)
     end
   end
